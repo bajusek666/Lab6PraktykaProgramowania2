fb762922687037d4d268f1615a0a511f713a2c4c
Kamil Bajus <kamil.bajus@onet.pl>
2024-05-15 18:47:05 +0200

    added documentation to functions

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 # przykladowe funkcje utils .py
 def add ( a: int , b: int) -> int:
+    """function to sum numbers"""
     return a + b
 def subtract ( a: int , b: int) -> int:
+    """function to substract numbers"""
     return a - b
 def multiply ( a: int , b: int) -> int:
+    """function to multiply numbers"""
     return a * b
 def divide ( a: int , b: int) -> float :
+    """function to divide numbers"""
     return a / b
